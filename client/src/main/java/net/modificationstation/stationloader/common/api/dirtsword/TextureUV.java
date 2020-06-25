b76fd78be43160e8dbb8cf3c29a7cd45db145b7f
biliam <yaboibiliam@gmail.com>
2020-06-25 04:43:35 -0500

    Added class for obj texturing to be made at a later date

---
+++ client/src/main/java/net/modificationstation/stationloader/common/api/dirtsword/TextureUV.java
@@ -0,0 +1,11 @@
+package net.modificationstation.stationloader.common.api.dirtsword;
+
+import org.lwjgl.opengl.GL11;
+public class TextureUV {
+	public TextureUV() {
+		
+	}
+	public void applyTexture() {
+		//opengl wizardry here at a later date
+	}
+}
